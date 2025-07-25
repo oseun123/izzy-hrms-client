a7aa9b43192747e087c102049405aacbce3a40c7
unknown <oseun04@gmail.com>
2025-07-25 13:57:41 +0100

    add: contact crud feature

--- src/store/actions/hrActions.js
+++ src/store/actions/hrActions.js
@@ -28,5 +28,18 @@ const updateEmployee = async (dispatch, request, creds) => {
     dispatch({ type: 'GENERIC_ERROR', payload: resMessage });
   }
 };
+const createContact = async (dispatch, request, creds) => {
+  try {
+    dispatch({ type: 'CLEAR_USERS_ERRORS' });
+    dispatch({ type: 'CLEAR_PREFERENCES_ERRORS' });
+    const result = await request.post('/hris/set-contact', creds);
 
-export { hrCleanUp, createEmployee, updateEmployee };
+    dispatch({ type: 'GENERIC_SUCCESS', payload: result.data });
+    return result.data;
+  } catch (error) {
+    const resMessage = error?.response?.data;
+    dispatch({ type: 'GENERIC_ERROR', payload: resMessage });
+  }
+};
+
+export { hrCleanUp, createEmployee, updateEmployee, createContact };
